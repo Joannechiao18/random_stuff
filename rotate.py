723562a5c2fe5692a8da7d0a0b10bbb6b783ed06
Joanne Wang <84509949+Joannechiao18@users.noreply.github.com>
2022-03-13 20:16:42 +0800

    Update rotate.py

--- rotate.py
+++ rotate.py
@@ -5,7 +5,7 @@ import math
 image = np.array(Image.open("C:/Users/joann/Desktop/5616.jpg"))
 
 ori_center_height = round(image.shape[0] / 2)
-ori_center_width = round(image.shape[1] / 2)  
+ori_center_width = round(image.shape[1] / 2)
 
 def rotate(array, angle_in_degrees):
     import itertools
@@ -18,21 +18,21 @@ def rotate(array, angle_in_degrees):
     new_height = round(abs(image.shape[0] * cosine) + abs(image.shape[1] * sine))
     new_width = round(abs(image.shape[1] * cosine) + abs(image.shape[0] * sine))
 
-    result = np.zeros(new_height, new_width, image.shape[2])
+    result = np.zeros((new_height, new_width, image.shape[2]))
 
     new_center_height = round(new_height / 2)
     new_center_width = round(new_width / 2)
 
     for i, j in itertools.product(range(w), range(h)):
-        y = image.shape[0] - 1 - j - ori_center_height
-        x = image.shape[1] - 1 - i - ori_center_width
+        y = j - ori_center_height
+        x = i - ori_center_width
         #pixels = array[y, x, :]
 
         new_x, new_y = np.matmul(rotation_matrix, [x, y])
         new_x, new_y = int(new_x), int(new_y)
 
-        new_y = new_center_height - new_y
-        new_x = new_center_width - new_x
+        new_y = new_center_height + new_y
+        new_x = new_center_width + new_x
 
         if 0 <= new_x < new_width and 0 <= new_y < new_height:
             result[new_y, new_x, :] = image[j, i,:]
